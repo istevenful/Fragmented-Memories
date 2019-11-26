fc18094a33ea9c264b4db1192d9cac9cec4f3b17
Steven Tan <steventangerine@gmail.com>
2019-11-26 02:00:26 -0800

    Created Scene Moving Script

---
+++ Fragmented Memories/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [SerializeField] private int SceneNumber;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneNumber);
+        }
+    }
+}
