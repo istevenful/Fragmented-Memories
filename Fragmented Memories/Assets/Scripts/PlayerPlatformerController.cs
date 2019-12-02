1af85ff996dce9183b56ea37f8ddaa21f7d78b87
Steven Tan <steventangerine@gmail.com>
2019-12-02 15:19:39 -0800

    Revert "Updated EnemyAI"
    This reverts commit d24837ae6a24b2d61fc1ea6214f58579c4bce43d.

--- Fragmented Memories/Assets/Scripts/PlayerPlatformerController.cs
+++ Fragmented Memories/Assets/Scripts/PlayerPlatformerController.cs
@@ -41,12 +41,10 @@ public class PlayerPlatformerController : PhysicsObject
         {
             spriteRenderer.flipX = !spriteRenderer.flipX;
         }
-        if (animator.gameObject.activeSelf)
-        {
-            // animator.SetBool("grounded", grounded);
-            // animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
-        }
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
         targetVelocity = move * maxSpeed;
     }
 }
