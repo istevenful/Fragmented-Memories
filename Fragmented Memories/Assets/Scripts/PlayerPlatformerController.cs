1fa0259cb5b293aadd1cd4b9a9dfaf7077122685
Ethan Chiang <ethanyichiang@gmail.com>
2019-12-04 20:17:28 -0800

    add attackbox

--- Fragmented Memories/Assets/Scripts/PlayerPlatformerController.cs
+++ Fragmented Memories/Assets/Scripts/PlayerPlatformerController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerPlatformerController : PhysicsObject
 {
     // ADSR
-    [SerializeField] private float MaxSpeed = 10.0f;
     [SerializeField] private float AttackDuration = 0.5f;
     [SerializeField] private AnimationCurve Attack;
     [SerializeField] private float DecayDuration = 0.25f;
@@ -18,11 +17,29 @@ public class PlayerPlatformerController : PhysicsObject
     private float DecayTimer;
     private float SustainTimer;
     private float ReleaseTimer;
-    private float InputDirection = 0.0f;
     private enum Phase { Attack, Decay, Sustain, Release, None };
     private Phase CurrentPhase;
     private float prevHorizontialAxisInput = 0f;
 
+    // Movement vars
+    [SerializeField] private float MaxSpeed = 10.0f;
+    [SerializeField] private float jumpTakeOffSpeed = 7;
+
+    // Jump forgiveness set to 0.5s
+    [SerializeField] private float jumpWindow = 0.5f;
+
+    // Air jump flag
+    private bool canAirJump = true;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Attack box
+    [SerializeField] private GameObject attabox;
+    private Vector3 flipVector = new Vector3(1.75f, 0, 0);
+
+    // ADSR implemetation
+    // Taken for Dr.Mccoy's class demo project
     private void ResetTimers()
     {
         this.AttackTimer = 0.0f;
@@ -71,17 +88,11 @@ public class PlayerPlatformerController : PhysicsObject
                 this.CurrentPhase = Phase.None;
             }
         }
-        //Debug.Log(velocity);
+        
         return velocity;
     }
 
-    //public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
-    private float jumpWindow = 0.5f;
-    private bool canAirJump = true;
-
-    private SpriteRenderer spriteRenderer;
-    private Animator animator;
+   
 
     // Use this for initialization
     void Awake()
@@ -98,9 +109,8 @@ public class PlayerPlatformerController : PhysicsObject
         move.x = Input.GetAxis("Horizontal");
 
         // Activate ADSR
-
         // GetButtonDown(Right)
-        if (this.prevHorizontialAxisInput == 0f && Input.GetAxis("Horizontal") > 0)
+        if (this.prevHorizontialAxisInput == 0.0f && Input.GetAxis("Horizontal") > 0.01f)
         {
             this.ResetTimers();
             this.CurrentPhase = Phase.Attack;
@@ -112,7 +122,7 @@ public class PlayerPlatformerController : PhysicsObject
         }
 
         // GetButtonDown(Left)
-        if (this.prevHorizontialAxisInput == 0f && Input.GetAxis("Horizontal") < 0)
+        if (this.prevHorizontialAxisInput == 0.0f && Input.GetAxis("Horizontal") < -0.01f)
         {
             this.ResetTimers();
             this.CurrentPhase = Phase.Attack;
@@ -125,6 +135,8 @@ public class PlayerPlatformerController : PhysicsObject
 
         this.prevHorizontialAxisInput = Input.GetAxis("Horizontal");
 
+
+        // Air jump
         this.jumpWindow -= Time.deltaTime;
 
         // Still in air 
@@ -146,7 +158,7 @@ public class PlayerPlatformerController : PhysicsObject
             canAirJump = true;
         }
 
-        // Forgiven jump
+        // Forgiving jump
         if(grounded && this.jumpWindow > 0f)
         {
             velocity.y = jumpTakeOffSpeed;
@@ -167,10 +179,11 @@ public class PlayerPlatformerController : PhysicsObject
             }
         }
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < -0.01f));
         if (flipSprite)
         {
             spriteRenderer.flipX = !spriteRenderer.flipX;
+            attabox.transform.position = attabox.transform.position + (flipVector *= -1f);
         }
 
         animator.SetBool("grounded", grounded);
