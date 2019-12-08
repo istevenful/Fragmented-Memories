f439a7ebc69bd2dd4ff88e9aa415b8a9b4fa9505
Steven Tan <steventangerine@gmail.com>
2019-12-08 04:33:13 -0800

    Scene Switching works in scene 1 and with loading screen

---
+++ Fragmented Memories/Assets/Scripts/LoadNewScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadNewScene : MonoBehaviour
+{
+    [SerializeField] private int LoadingSceneNumber;
+    public Slider slider;
+    private float number = 0;
+    public Text percentage;
+    void Update()
+    {
+        if (number < 100)
+        {
+            float multiplier = Random.Range(0f, .7f);
+            number += Time.deltaTime * multiplier;
+            slider.value = number;
+            percentage.text = (int)(number * 100) + "%";
+
+        }
+        if (slider.value > .99)
+        {
+            Debug.Log(slider.value);
+            SceneManager.LoadScene(LoadingSceneNumber);
+        }
+    }
+
+}
