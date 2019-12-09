58d743f9b474c12c1e0723d1becec39f2f8cd9ce
Steven Tan <steventangerine@gmail.com>
2019-12-08 16:17:17 -0800

    Maps Updated
    Waiting for Jack to add some stuff and hitboxes.

---
+++ Fragmented Memories/Assets/Scripts/SceneDarkener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SceneDarkener : MonoBehaviour
+{
+    public Image darkScreen;
+    [SerializeField] private float Alpha;
+    [SerializeField] private float MaxAlpha;
+    void Update()
+    {
+        if(Alpha < MaxAlpha)
+        {
+            Alpha += .0002f;
+
+            darkScreen.color = new Color(darkScreen.color.r, darkScreen.color.g, darkScreen.color.b, Alpha);
+        }
+
+        
+    }
+}
