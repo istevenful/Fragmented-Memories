d7d2a5683d363fa4a5ac5b90b14bc83c77929608
Steven Tan <steventangerine@gmail.com>
2019-12-09 18:55:40 -0800

    l

---
+++ Fragmented Memories/Assets/Scripts/Dead.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead : MonoBehaviour
+{
+    private Animator anim;
+    bool dead = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        this.anim.SetBool("Dead", true);
+        anim.SetFloat("Speed", 0);
+        dead = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (!dead)
+            {
+                this.anim.SetBool("Dead", true);
+                anim.SetFloat("Speed", 0);
+                dead = true;
+            }
+            else
+            {
+                anim.SetBool("Dead", false);
+                dead = false;
+            }
+        }
+    }
+}
